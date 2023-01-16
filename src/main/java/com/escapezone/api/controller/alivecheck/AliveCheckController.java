849c72d1650c8cb181c5006f0a3f235da7a40849
yeojin <lyeo7272@gmail.com>
2023-01-16 20:35:13 +0900

    alivecheck 테스트용 바디 추가

--- src/main/java/com/escapezone/api/controller/alivecheck/AliveCheckController.java
+++ src/main/java/com/escapezone/api/controller/alivecheck/AliveCheckController.java
@@ -1,13 +1,18 @@
 package com.escapezone.api.controller.alivecheck;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://local.escapezone.com:3000")
+@Slf4j
 public class AliveCheckController {
     @GetMapping("/alive")
-    public void aliveCheck(){
-    }
+    public TestDto aliveCheck(){
+        log.debug("aliveCheck");
+   return TestDto.builder().test("test").build(); }
 
 }
---
+++ src/main/java/com/escapezone/api/controller/alivecheck/TestDto.java
@@ -0,0 +1,10 @@
+package com.escapezone.api.controller.alivecheck;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class TestDto {
+    private String test;
+}
